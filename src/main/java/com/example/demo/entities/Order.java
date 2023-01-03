c490451a45dff6c05872b9789c6da3510838b0fb
diego <diego.pavan05@gmail.com>
2023-01-03 02:01:38 -0300

    Payment, one-to-one association

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -8,7 +8,9 @@ import java.util.Set;
 
 import com.example.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +43,11 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	Set<OrderItem>items = new HashSet<>();
+	
+	
+	@OneToOne(mappedBy = "order",cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	
 	public Order() {
 		
@@ -76,9 +84,16 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
 
-	
-	
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -89,6 +104,9 @@ public class Order implements Serializable {
 		}
 		
 	}
+	
+	
+	
 	
 	public Set<OrderItem> getItems(){
 		return items;
