5f6833f14a267361ffcf8f9f57e52f2e27a3dc8d
diego <diego.pavan05@gmail.com>
2023-01-03 07:27:48 -0300

    upgrade product

--- src/main/java/com/example/demo/resources/ProductResource.java
+++ src/main/java/com/example/demo/resources/ProductResource.java
@@ -1,13 +1,19 @@
 package com.example.demo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.entities.Product;
 import com.example.demo.services.ProductService;
@@ -25,10 +31,30 @@ public class ProductResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id){
-		Product obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		Product product  = service.findById(id);
+		return ResponseEntity.ok().body(product);
+	} 
+	
+	@PostMapping
+	public ResponseEntity<Product> insert(@RequestBody Product product) {
+		product = service.insert(product);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId()).toUri();
+		return ResponseEntity.created(uri).body(product);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product){
+		product = service.update(id, product);
+		return ResponseEntity.ok().body(product);
 	}
 	
 	
--- src/main/java/com/example/demo/services/ProductService.java
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -4,24 +4,61 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Product;
 import com.example.demo.repositories.ProductRepository;
+import com.example.demo.services.exceptions.DatabaseException;
+import com.example.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
-	
+
 	public Product findById(Long id) {
-		Optional<Product> obj = repository.findById(id);
-		return obj.get();
+		Optional<Product> product = repository.findById(id);
+		return product.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
-	
-	public List<Product> findAll(){
+
+	public List<Product> findAll() {
 		return repository.findAll();
 	}
-	
+
+	public Product insert(Product orderItem) {
+		return repository.save(orderItem);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Product update(Long id, Product product) {
+		try {
+			Product entity = repository.getReferenceById(id);
+			updateData(entity, product);
+			return entity;
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public void updateData(Product entity, Product product) {
+		entity.setName(product.getName());
+		entity.setDescription(product.getDescription());
+		entity.setPrice(product.getPrice());
+		entity.setImgUrl(product.getImgUrl());
+	}
+
 }
