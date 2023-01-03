02dad1be5d55278ccb7f78aec07c79f98fa11075
diego <diego.pavan05@gmail.com>
2023-01-03 08:12:12 -0300

    category upgrade

--- src/main/java/com/example/demo/services/CategoryService.java
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entities.Category;
 import com.example.demo.repositories.CategoryRepository;
+import com.example.demo.services.exceptions.DatabaseException;
+import com.example.demo.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -15,14 +21,44 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public Category findyById(Long id) {
-		Optional<Category> obj = repository.findById(id);
-		return obj.get();
-	}
-	
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
 	
+	public Category findyById(Long id) {
+		Optional<Category> category = repository.findById(id);
+		return category.orElseThrow(()-> new ResourceNotFoundException(id));
+	}
+	
+	public Category insert(Category category) {
+		return repository.save(category);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Category update(Long id, Category category) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			updateData(entity,category);
+			return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	public void updateData(Category entity,Category category) {
+		entity.setName(category.getName());
+	}
 	
 }
